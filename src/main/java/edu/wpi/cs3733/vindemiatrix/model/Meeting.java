12333a7e3e3e6940cd2e0b8196a65c086e376fc8
Mikayla Fischler <micron1314@gmail.com>
2018-11-27 17:36:09 -0500

    models implemented

--- src/main/java/edu/wpi/cs3733/vindemiatrix/model/Meeting.java
+++ src/main/java/edu/wpi/cs3733/vindemiatrix/model/Meeting.java
@@ -1,5 +1,16 @@
 package edu.wpi.cs3733.vindemiatrix.model;
 
 public class Meeting {
-
+	public final int id;
+	public final String participant;
+	
+	/**
+	 * Create a meeting object to represent a meeting in a time slot.
+	 * @param id The meeting ID
+	 * @param participant The secret code for the participant to gain access
+	 */
+	public Meeting(int id, String participant) {
+		this.id = id;
+		this.participant = participant;
+	}
 }

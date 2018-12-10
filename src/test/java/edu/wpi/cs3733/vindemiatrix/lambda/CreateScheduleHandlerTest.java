233d37734d7988480eaeac370848eecec94e6284
Mikayla Fischler <micron1314@gmail.com>
2018-12-10 16:21:45 -0500

    thorough test of create schedule

--- src/test/java/edu/wpi/cs3733/vindemiatrix/lambda/CreateScheduleHandlerTest.java
+++ src/test/java/edu/wpi/cs3733/vindemiatrix/lambda/CreateScheduleHandlerTest.java
@@ -5,6 +5,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -15,6 +17,7 @@ import org.junit.Test;
 import com.google.gson.Gson;
 
 import edu.wpi.cs3733.vindemiatrix.lambda.request.CreateScheduleRequest;
+import edu.wpi.cs3733.vindemiatrix.lambda.response.CreateScheduleResponse;
 
 public class CreateScheduleHandlerTest {
 
@@ -45,16 +48,28 @@ public class CreateScheduleHandlerTest {
 			e.printStackTrace();
 		}
 		
-
-		CreateScheduleRequest request = new Gson().fromJson((String) response.get("body"), CreateScheduleRequest.class);
+		CreateScheduleResponse resp = new Gson().fromJson((String) response.get("body"), CreateScheduleResponse.class);
 		
-		Assert.assertEquals("Test Organizer", request.name);
-		Assert.assertEquals("2019-10-29", request.start_date);
-		Assert.assertEquals("2019-12-29", request.end_date);
-		Assert.assertEquals("08:00", request.start_time);
-		Assert.assertEquals("14:00", request.end_time);
-		Assert.assertEquals(60, request.meeting_duration);
-		Assert.assertEquals(1, request.default_open);
+		Assert.assertEquals("Test Organizer", resp.name);
+		Assert.assertEquals("2019-10-29", resp.start_date);
+		Assert.assertEquals("2019-12-29", resp.end_date);
+		Assert.assertEquals("08:00", resp.start_time);
+		Assert.assertEquals("14:00", resp.end_time);
+		Assert.assertEquals(60, resp.meeting_duration);
+		Assert.assertEquals("Schedule(" + resp.secret_code + "," + resp.name + "," + resp.id + ")", resp.toString());
+		
+		CreateScheduleResponse resp2 = new CreateScheduleResponse(400);
+		Assert.assertEquals(400, resp2.httpCode);
+		
+		CreateScheduleResponse resp3 = new CreateScheduleResponse(400, "Bad Input");
+		Assert.assertEquals(400, resp3.httpCode);
+		Assert.assertEquals("Bad Input", resp3.error);
+		Assert.assertEquals("NoSchedule", resp3.toString());
+
+		Pattern p = Pattern.compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
+		Matcher m = p.matcher(resp.secret_code);
+		
+		Assert.assertTrue(m.find());
 
         String sampleOutputString = output.toString();
         System.out.println(sampleOutputString);
